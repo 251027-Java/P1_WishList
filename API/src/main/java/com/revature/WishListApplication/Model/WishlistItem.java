4d9e0a4397c6be31622b8a41713d5ee0075de494
BrodyRoche <rochebrody4@gmail.com>
2025-12-03 23:40:38 -0600

    Application runs, connects to db, Users added successfully

--- API/src/main/java/com/revature/WishListApplication/Model/WishlistItem.java
+++ API/src/main/java/com/revature/WishListApplication/Model/WishlistItem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.MapsId;
 import lombok.Data;
 import lombok.ToString;
 
@@ -11,11 +13,16 @@ import lombok.ToString;
 @Table(name = "WishlistItem")
 @Data
 public class WishlistItem {
+    @EmbeddedId
+    private WishlistItemId id;
+
+    @MapsId("wishlistId")
     @ManyToOne()
     @JoinColumn(name = "wishlistId")
     @ToString.Exclude
     private Wishlist wishlist;
 
+    @MapsId("itemId")
     @ManyToOne()
     @JoinColumn(name = "itemId")
     @ToString.Exclude
--- API/src/main/java/com/revature/WishListApplication/Service/ItemService.java
+++ API/src/main/java/com/revature/WishListApplication/Service/ItemService.java
@@ -22,7 +22,7 @@ public class ItemService {
     }
 
     public List<ItemDTO> searchByItemname(String name){
-        return repository.findByName(name).stream().map(this::ItemToDto).toList();
+        return repository.findByItemName(name).stream().map(this::ItemToDto).toList();
     }
 
     public ItemDTO create(ItemWOIDDTO dto){
