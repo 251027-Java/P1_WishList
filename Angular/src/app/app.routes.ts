ec15597d379975cd20814f3a9e03a284daa94c79
manas <manasvini569@revature.net>
2025-12-12 14:37:44 -0600

    Added functionality to log in and made homepage public

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { Auth } from './services/auth';
 import { ViewWishlistComponent } from './components/view-wishlist/view-wishlist.component';
 import { EditWishlistComponent } from './components/edit-wishlist/edit-wishlist.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -14,22 +15,21 @@ export const routes: Routes = [
     {
         path:"dashboard",
         component:DashboardComponent,
-        canMatch: [Auth]
+        canMatch: [authGuard]
     },
     {
         path:"view-wishlists",
         component:ViewWishlistComponent,
-        canMatch: [Auth]
+        canMatch: [authGuard]
     },
     {
         path:"edit-wishlist",
         component:EditWishlistComponent,
-        canMatch: [Auth]
+        canMatch: [authGuard]
     },
     {
         path:"home",
-        component:HomeComponent,
-        canMatch: [Auth]
+        component:HomeComponent
     },
     {
         path: '**',
--- Angular/src/app/components/login-signup/login-signup.component.ts
+++ Angular/src/app/components/login-signup/login-signup.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login-signup',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.css'
 })
 export class LoginSignupComponent {
-  
+  username:string = ""
+  password:string = ""
 
+  constructor(private router:Router, private auth:Auth) {}
+  
+  login() {
+    // send username and password to api
+
+    if (this.username === 'user' && this.password === '1234') {
+      alert("username and password accepted");
+      this.auth.authenticateUser();
+      this.router.navigateByUrl("dashboard");
+    }
+  }
 }
