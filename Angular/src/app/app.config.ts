a3cc9a3c21ccfd50024bcf29748afc8ba988b148
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2025-12-12 13:37:48 -0600

    Developed the Individual wishlist page

--- Angular/src/app/app.config.ts
+++ Angular/src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {WishlistComponent} from './components/wishlist/wishlist.component';
 
 import { routes } from './app.routes';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
