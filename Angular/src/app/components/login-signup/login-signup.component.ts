f2f8a3050ed1fd1fd00d2572ce77c0f038e4333b
manas <manasvini569@revature.net>
2025-12-15 12:59:13 -0600

    Login functionality with CORS error

--- Angular/src/app/components/login-signup/login-signup.component.ts
+++ Angular/src/app/components/login-signup/login-signup.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Auth } from '../../services/auth';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../../interfaces/user';
+import { map, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-login-signup',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.css'
@@ -12,16 +17,44 @@ import { Auth } from '../../services/auth';
 export class LoginSignupComponent {
   username:string = ""
   password:string = ""
+  checkName:string = "";
+  checkPassword:string = "";
+  loginStatus:boolean = true;
 
-  constructor(private router:Router, private auth:Auth) {}
+  constructor(private router:Router, private auth:Auth, private http:HttpClient) {}
   
   login() {
     // send username and password to api
-
-    if (this.username === 'user' && this.password === '1234') {
-      alert("username and password accepted");
-      this.auth.authenticateUser();
-      this.router.navigateByUrl("dashboard");
+    this.findUser().subscribe({next: data => {
+      this.checkName = data.userUsername
+      this.checkPassword = data.userPassword;
+      if (this.username === this.checkName && this.password === this.checkPassword) {
+        alert("username and password accepted");
+        this.auth.authenticateUser();
+        this.router.navigateByUrl("dashboard");
+      }
+      else
+      {
+        this.loginStatus = false;
+      }
+    },
+    error: () => {
+      this.loginStatus = false;
     }
+  });
+  }
+
+  findUser():Observable<User> {
+    const headers = new HttpHeaders({
+      Authorization: `Basic ${this.username}:${this.password}`
+    });
+
+    return this.http.get<User>(`http://localhost:8080/api/users/search?username=${this.username}`, { headers }).pipe(
+      map<any, User>(data => ({
+        userId:data.userId,
+        userUsername:data.userUsername,
+        userPassword:data.userPassword
+      }))
+    );
   }
 }
