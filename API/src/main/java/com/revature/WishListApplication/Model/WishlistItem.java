b94ffb562e9bea8e0baa6d8a0c3374b626221695
manas <manasvini569@revature.net>
2025-12-16 00:12:06 -0600

    Changed to WishListItemID

--- API/src/main/java/com/revature/WishListApplication/Model/WishlistItem.java
+++ API/src/main/java/com/revature/WishListApplication/Model/WishlistItem.java
@@ -10,7 +10,7 @@ import lombok.Data;
 import lombok.ToString;
 
 @Entity
-@IdClass(WishlistItemId.class)
+@IdClass(WishlistItemID.class)
 @Table(name = "WishlistItem")
 @Data
 public class WishlistItem {
