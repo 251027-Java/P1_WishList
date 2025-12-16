b34a3041deff154f2c8c0255ea917adca613d2e7
manas <manasvini569@revature.net>
2025-12-15 22:31:49 -0600

    Reverting most changes

--- API/src/main/java/com/revature/WishListApplication/WebMvcConfig.java
+++ API/src/main/java/com/revature/WishListApplication/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOriginPatterns("http://localhost:4200")
                     .allowCredentials(true)
                     .allowedHeaders("*")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
